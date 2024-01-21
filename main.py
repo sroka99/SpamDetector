5df1fea046beff1e3bf9157389d455a1c868c947
sroka99 <srokaszymon001@gmail.com>
2024-01-22 00:38:46 +0100

    Starting_items

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
